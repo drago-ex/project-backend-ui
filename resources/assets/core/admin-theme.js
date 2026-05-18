757b94690370ea01fb50282c6f3d01f6b98f4249
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-18 12:22:23 +0200

    Update

--- resources/assets/core/admin-theme.js
+++ resources/assets/core/admin-theme.js
@@ -2,13 +2,6 @@ import PerfectScrollbar from "perfect-scrollbar";
 import { SidebarSkeleton  } from "sidebar-skeleton-compostrap";
 import { SidebarMenuApp } from "sidebar-menu-compostrap";
 
-import "sidebar-skeleton-compostrap/sidebar.css";
-import "sidebar-menu-compostrap/sidebar-menu.css";
-import "sidebar-menu-2-compostrap/dist/sidebar-custom.css";
-import "dashboard-skeleton-compostrap/dist/dashboard.css";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "perfect-scrollbar/css/perfect-scrollbar.css";
-
 export function initAdminTheme() {
 	SidebarSkeleton.init();
 	SidebarMenuApp.init();
