9f4362fd220247cc443012db65fb6b63392ba554
accgit <zdenek.papucik@gmail.com>
2026-06-06 07:29:50 +0200

    Update

--- resources/assets/core/admin-theme.js
+++ resources/assets/core/admin-theme.js
@@ -1,14 +1,19 @@
 import PerfectScrollbar from "perfect-scrollbar";
-import { SidebarSkeleton  } from "sidebar-skeleton-compostrap";
+import { DashboardSkeleton } from "dashboard-skeleton-compostrap";
+import { Sidebar } from "sidebar-skeleton-compostrap";
 import { SidebarMenuApp } from "sidebar-menu-compostrap";
 
 export function initAdminTheme() {
-	SidebarSkeleton.init();
+	Sidebar.init();
 	SidebarMenuApp.init();
+	DashboardSkeleton.init();
 	initScrollbar('.scrollbar');
 }
 
 /* Function to initialize PerfectScrollbar */
 function initScrollbar(selector) {
-	new PerfectScrollbar(selector, { wheelSpeed: 0.3 });
+	const element = document.querySelector(selector);
+	if (element) {
+		new PerfectScrollbar(element, { wheelSpeed: 0.3 });
+	}
 }
