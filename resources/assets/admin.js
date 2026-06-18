50564f455c85c8024066ceb075fa26c524ab4022
accgit <zdenek.papucik@gmail.com>
2026-06-18 08:27:19 +0200

    Update

--- resources/assets/admin.js
+++ resources/assets/admin.js
@@ -1,6 +1,11 @@
 // these JS + SCSS will be automatically available after installing the package
 import { registerNajaExtensions } from "./core/base.js";
 import { initAdminTheme } from "./core/admin-theme.js";
+
+// drago-ex extensions
+import { BootstrapDropdowns } from "drago-component";
+
+// page styles
 import "./admin.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -8,5 +13,5 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 registerNajaExtensions(
-	// naja extensions
+	BootstrapDropdowns
 );
