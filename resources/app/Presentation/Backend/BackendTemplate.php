18681f6ed4ce704bd3da8a7c275db8aa3ed78702
accgit <zdenek.papucik@gmail.com>
2026-06-23 19:31:34 +0200

    Update

--- resources/app/Presentation/Backend/BackendTemplate.php
+++ resources/app/Presentation/Backend/BackendTemplate.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\Presentation\Backend;
 
-use App\Core\User\UserAccess;
+use App\Core\Security\CurrentUser;
 use App\Presentation\Backend\Accessory\Menu\SidebarItem;
 use App\Presentation\BaseTemplate;
 
 
 class BackendTemplate extends BaseTemplate
 {
-	public UserAccess $userAccess;
+	public CurrentUser $currentUser;
 
 	/** @var array<string, SidebarItem[]> */
 	public array $sidebarMenu = [];
